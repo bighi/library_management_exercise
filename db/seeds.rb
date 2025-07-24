e6fd10bca2407ae38faf5b59cbc24fb0aa1fdef4
bighi <contato@leonardobighi.com>
2025-07-24 14:34:59 -0300

    Add many borrowings to the seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,12 +8,34 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-User.create email: "member@email.com", password: "test123",
+member = User.create email: "member@email.com", password: "test123",
   password_confirmation: "test123", role: :member
-User.create email: "librarian@email.com", password: "test123",
+librarian = User.create email: "librarian@email.com", password: "test123",
   password_confirmation: "test123", role: :librarian
 
+books = []
+
 30.times do
-  Book.create title: Faker::Book.title, author: Faker::Book.author,
-    genre: Faker::Book.genre, isbn: rand(11111111111..99999999999), total_copies: rand(2..8)
+  books < Book.create(title: Faker::Book.title, author: Faker::Book.author,
+    genre: Faker::Book.genre, isbn: rand(11111111111..99999999999), total_copies: rand(2..8))
+end
+
+# Overdue borrowing
+Borrowing.create book: books.sample, user: member, borrowed_at: 15.days.ago,
+  due_at: 3.days.ago
+
+# Returned borrowings
+Borrowing.create book: books.sample, user: member, borrowed_at: 15.days.ago,
+  due_at: 3.days.ago, returned_at: 6.days.ago
+Borrowing.create book: books.sample, user: member, borrowed_at: 28.days.ago,
+  due_at: 14.days.ago, returned_at: 20.days.ago
+Borrowing.create book: books.sample, user: member, borrowed_at: 55.days.ago,
+  due_at: 43.days.ago, returned_at: 28.days.ago
+
+# Active borrowings
+3.times do
+  borrow_date = rand(2..11).days.ago
+
+  Borrowing.create book: books.sample, user: member, borrowed_at: borrow_date,
+    due_at: borrow_date + 12.days
 end
