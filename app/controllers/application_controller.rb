b3e33d0542958a0d607318827f56097ccaa1a579
bighi <contato@leonardobighi.com>
2025-07-25 11:20:10 -0300

    Returns 403 forbidden when trying to do an unauthorized action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,15 @@ class ApplicationController < ActionController::Base
   allow_browser versions: :modern
 
   include Pundit::Authorization
+
+  rescue_from Pundit::NotAuthorizedError do |exception|
+    respond_to do |format|
+      format.html do
+        redirect_to(request.referrer || root_path, alert: "You are not authorized to perform this action.")
+      end
+      format.json do
+        render json: { error: "You are not authorized to perform this action." }, status: :forbidden
+      end
+    end
+  end
 end
